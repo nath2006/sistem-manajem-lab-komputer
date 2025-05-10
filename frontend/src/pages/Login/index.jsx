e320e4dc5e7f2231f436817a150dced6ed537b71
Nath2006 <ananthapindika739@gmail.com>
2025-05-11 00:16:39 +0700

    Implement code changes to enhance functionality and improve performance

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,29 +1,29 @@
 // pages/LoginPage.js
 import React from "react";
+import {Newspaper} from "lucide-react";
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <div className="flex flex-col md:flex-row flex-1 justify-center mx-4 md:mx-0">
-        <div className="hidden md:flex md:w-2/3 bg-red-900 items-center justify-center">
-          <img
-            className="max-w-lg"
-            src="/assets/images/login/login-illustration.svg" 
-            alt="Login Illustration"
-          />
-        </div>
-
-        <div className="flex flex-col w-full md:w-1/2 lg:w-1/3 bg-white p-8 md:p-16 justify-center rounded-xl shadow-lg md:rounded-none md:shadow-none">
+      <div className="flex flex-col md:flex-row flex-1  mx-4 md:mx-0">
+        <div className="flex flex-col w-full md:w-1/2 lg:w-2/3 bg-white p-8 md:p-16 justifyrounded-xl shadow-lg md:rounded-none md:shadow-none">
           <div className="mb-8 text-left">
-            <h2 className="text-base md:text-2xl font-bold text-red-900">
-              Sistem Informasi Manajemen Laboratorium Komputer
-            </h2>
-            <p className="mt-2 text-gray-600">
-              Silakan masukan username dan password anda
-            </p>
+            <div className="headerWrapper align-start">
+              <div className="h-24 w-24 mb-4">
+                <img src="/assets/images/login/logo.png" alt="" />
+              </div>
+              <h2 className="text-base md:text-xl font-bold text-red-900 max-w-lg">
+                Selamat Datang di Sistem Informasi Manajemen Laboratorium
+                Komputer
+              </h2>
+            </div>
+            <div className="mt-12">
+              <p className="mt-2  text-gray-900 font-bold text-2xl">Login</p>
+              <p className="mt-2  text-gray-600">Lengkapi data berikut ini !</p>
+            </div>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4 max-w-lg">
             <div>
               <label
                 htmlFor="login-username"
@@ -63,6 +63,32 @@ const LoginPage = () => {
               Masuk
             </button>
           </form>
+        </div>
+
+        <div className="hidden md:flex md:w-1/2 bg-red-900 items-center p-12">
+          {/* <img
+            className="max-w-lg"
+            src="/assets/images/login/login-illustration.svg" 
+            alt="Login Illustration"
+          /> */}
+          <div className="flex flex-col gap-8">
+            <div className="">
+              {/* <div className="bg-white">
+                <Newspaper color="red" size={34}/>
+              </div> */}
+            <h1 className="text-base md:text-3xl font-bold text-white">
+              Pengumuman Terbaru
+            </h1>
+            <p className="text-based md:text-md text-white pt-1">Update terbaru penggunaan laboratorium komputer</p>
+            </div>
+            <div className="CardWrapper pl-8">
+              <div className="h-[550px] w-[400px] bg-white rounded-lg">
+              <div className="ImageWrapper bg-gray-900 w-full h-[300px] rounded-t-lg">
+                {/* Isi gambar atau konten lain di sini */}
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
