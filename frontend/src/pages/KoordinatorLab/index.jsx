e636dbb89a39202c9fdf39b35a86b26b58e0c978
Nath2006 <ananthapindika739@gmail.com>
2025-05-31 19:59:16 +0700

    feat: Add useTitle hook for dynamic page titles in Kepala Lab, Koordinator Lab, and Teknisi pages

--- frontend/src/pages/KoordinatorLab/index.jsx
+++ frontend/src/pages/KoordinatorLab/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Dashboard from '../../template/Dashboard'
+import useTitle from '../../utils/useTitle'
 
 export default function KepalaLabPage() {
+  useTitle("Dashboard Koordinator Lab - SIM Lab Komputer");
   return (
     <>
     <Dashboard> </Dashboard>
--- frontend/src/pages/Teknisi/index.jsx
+++ frontend/src/pages/Teknisi/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Dashboard from '../../template/Dashboard'
+import useTitle from '../../utils/useTitle'
 
 export default function KepalaLabPage() {
+  useTitle("Dashboard Teknisi - SIM Lab Komputer");
   return (
     <>
     <Dashboard> </Dashboard>
