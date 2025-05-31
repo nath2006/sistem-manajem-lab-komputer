ceb4b3026ded4d2edce984e1230a76748ac2a21c
Nath2006 <ananthapindika739@gmail.com>
2025-05-31 20:06:04 +0700

    feat: Refactor Dashboard import paths and remove deprecated template component

--- frontend/src/pages/KepalaLab/KepalaLabDashboard.jsx
+++ frontend/src/pages/KepalaLab/KepalaLabDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dashboard from '../../template/Dashboard'
+import Dashboard from '../../Layouts/Dashboard'
 import useTitle from '../../utils/useTitle'
 export default function KepalaLabPage() {
     useTitle("Dashboard Kepala Lab - SIM Lab Komputer");
